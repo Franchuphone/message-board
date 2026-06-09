91346e48f87cd1ad3b79d2e88fb819e312518f13
Franchuphone <222453306+Franchuphone@users.noreply.github.com>
2026-06-09 16:14:32 +0200

    fix: bug on database init

--- db/populateDb.js
+++ db/populateDb.js
@@ -1,32 +1,42 @@
 require("dotenv").config();
 const { Client } = require("pg");
 
-const SQL = `
-CREATE TABLE IF NOT EXISTS messages (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username VARCHAR ( 255 ) NOT NULL,
-  message TEXT,
-  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-);
+const createTableSQL = `
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR ( 255 ) NOT NULL,
+    message TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+  );
+`;
 
-TRUNCATE TABLE messages RESTART IDENTITY CASCADE;
-
-INSERT INTO messages (username, message) 
-VALUES
-  ('Daniel', 'La vaquitaaaaaaa'),
-  ('Yoyo', 'Puta'),
-  ('Nat', 'WTF !!! 😂😂😂');
+const seedDataSQL = `
+  INSERT INTO messages (username, message) 
+  VALUES
+    ('Daniel', 'La vaquitaaaaaaa'),
+    ('Yoyo', 'Puta'),
+    ('Nat', 'WTF !!! 😂😂😂')
+  ON CONFLICT DO NOTHING;
 `;
 
 async function main() {
-  console.log("seeding...");
+  console.log("🔧 Initialisation de la base de données...");
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
   });
   await client.connect();
-  await client.query(SQL);
+  await client.query(createTableSQL);
+  console.log("✅ Structure de la table vérifiée.");
+  const { rows } = await client.query("SELECT id FROM messages LIMIT 1");
+  if (rows.length === 0) {
+    console.log("🌱 Table vide. Insertion des données initiales...");
+    await client.query(seedDataSQL);
+    console.log("✅ Données initiales insérées.");
+  } else {
+    console.log("💾 Chargement de la table existante");
+  }
   await client.end();
-  console.log("done");
+  console.log("🎉 Initialisation de la table terminée.");
 }
 
 main();
